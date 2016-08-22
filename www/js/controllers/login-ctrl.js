1aeed11619b96ad5cf2a214652e6a1990cd54c56
Thiago Antune <thiago.antunes@outlook.com>
2016-08-22 00:53:11 -0300

    Login nativo facebook

--- www/js/controllers/login-ctrl.js
+++ www/js/controllers/login-ctrl.js
@@ -14,21 +14,35 @@ angular.module('main')
     function facebookLogin() {
       $ionicLoading.show({ template: 'Carregando...' });
       if (window.cordova) {
-        $ionicPlatform.ready(function () {
-          $cordovaOauth.facebook('1834143436814148', ['email', 'user_friends', 'public_profile']).then(function (result) {
-            var provider = new firebase.auth.FacebookAuthProvider();
-            provider.addScope('user_friends');
-            provider.addScope('email');
-            provider.addScope('public_profile');
-            var credential = firebase.auth.FacebookAuthProvider.credential(result.access_token);
-            firebase.auth().signInWithCredential(credential).then(function () {
-              $ionicLoading.hide();
-            });
-          }, function (error) {
+        // $ionicPlatform.ready(function () {
+        //   $cordovaOauth.facebook('1834143436814148', ['email', 'user_friends', 'public_profile']).then(function (result) {
+        //     var provider = new firebase.auth.FacebookAuthProvider();
+        //     provider.addScope('user_friends');
+        //     provider.addScope('email');
+        //     provider.addScope('public_profile');
+        //     var credential = firebase.auth.FacebookAuthProvider.credential(result.access_token);
+        //     firebase.auth().signInWithCredential(credential).then(function () {
+        //       $ionicLoading.hide();
+        //     });
+        //   }, function (error) {
+        //     $ionicLoading.hide();
+        //     $window.alert('Ops! Ocorreu um erro ao entrar no Rei da Quadra');
+        //     console.log("Error -> " + error);
+        //   });
+        // });
+        facebookConnectPlugin.login(['email', 'user_friends', 'public_profile'], function (result) {
+          var provider = new firebase.auth.FacebookAuthProvider();
+          provider.addScope('user_friends');
+          provider.addScope('email');
+          provider.addScope('public_profile');
+          var credential = firebase.auth.FacebookAuthProvider.credential(result.authResponse.accessToken);
+          firebase.auth().signInWithCredential(credential).then(function () {
             $ionicLoading.hide();
-            $window.alert('Ops! Ocorreu um erro ao entrar no Rei da Quadra');
-            console.log("Error -> " + error);
           });
+        }, function (error) {
+          $ionicLoading.hide();
+          $window.alert('Ops! Ocorreu um erro ao entrar no Rei da Quadra');
+          console.log("Error -> " + error);
         });
       } else {
         $facebook.login().then(function (result) {
@@ -44,17 +58,26 @@ angular.module('main')
       $ionicLoading.show({ template: 'Carregando...' });
       if (window.cordova) {
         $ionicPlatform.ready(function () {
-          $cordovaOauth.google('276187195050-hs1p1pa1dkh7ia7v2th8t55u7a89bgkq.apps.googleusercontent.com', ['profile', 'email']).then(function (result) {
-            var credential = firebase.auth.GoogleAuthProvider.credential(result.access_token);
-            firebase.auth().signInWithCredential(credential).then(function () {
+          window.plugins.googleplus.login(
+            {
+              'scopes': '', 
+              'webClientId': '276187195050-hs1p1pa1dkh7ia7v2th8t55u7a89bgkq.apps.googleusercontent.com', 
+              'offline': true,
+            },
+            function (obj) {
+              console.log(obj);
+              var credential = firebase.auth.GoogleAuthProvider.credential(obj.idToken);
+              firebase.auth().signInWithCredential(credential).then(function () {
+                $ionicLoading.hide();
+              });
+              console.log(result);
+            },
+            function (msg) {
               $ionicLoading.hide();
-            });
-            console.log(result);
-          }, function (error) {
-            $ionicLoading.hide();
-            $window.alert('Ops! Ocorreu um erro ao entrar no Rei da Quadra');
-            console.log("Error -> " + error);
-          });
+              $window.alert('Ops! Ocorreu um erro ao entrar no Rei da Quadra');
+              console.log("Error -> " + error);
+            }
+          );
         });
       }
     }
